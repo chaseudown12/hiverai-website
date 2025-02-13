89dca73ab589c88c652f5a66bd6fa484fb3ac250
chaseudown12 <owenchase1137@gmail.com>
2025-02-13 18:01:54 -0500

    Update LandingPage.js 2

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,1 +1,25 @@
 
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        backgroundImage: "url('https://your-domain.com/owyn-landing-background.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+        fontSize: "2rem",
+      }}
+    >
+      <h1>Welcome to Owyn</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
